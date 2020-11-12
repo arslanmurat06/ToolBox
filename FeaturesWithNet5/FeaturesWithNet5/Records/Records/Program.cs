c46c6b3d9b1be0326322682c918e47950978998f
unknown <mrtarsln06@gmail.com>
2020-11-12 17:05:50 +0300

    records with net5

---
+++ FeaturesWithNet5/FeaturesWithNet5/Records/Records/Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+
+namespace Records
+{
+    /// <summary>
+    /// Records
+    /// </summary>
+
+    ////////////////////Part -1 \\\\\\\\\\\\\\\\\\\\\\\\
+    //without constructor
+    public record Person
+    {
+        public string Name { get; set; }
+        public string Surname { get; set; }
+    }
+
+
+    ////////////////////Part -2 \\\\\\\\\\\\\\\\\\\\\\\\
+    //with constructor and immutable properties
+
+    public record Human
+    {
+        public string Name { get; init; }
+        public string Surname { get; init; }
+
+        public Human(string name, string surname)
+        {
+            Name = name;
+            Surname = surname;
+        }
+    }
+
+    class Program
+    {
+        
+        static void Main(string[] args)
+        {
+            ////////////////////Part -1 \\\\\\\\\\\\\\\\\\\\\\\\
+
+            Person person = new Person { Name = "Murat", Surname = "Arslan" };
+
+            //it has tostring itself
+            Console.WriteLine(person); // => output Person { Name = Murat, Surname = Arslan }
+
+            //we can create new record with only name different by using with keyword
+            Person person1 = person with { Name = "Hasan" };
+            Console.WriteLine(person1); // => output Person { Name = Hasan, Surname = Arslan }
+
+
+
+            ////////////////////Part -2 \\\\\\\\\\\\\\\\\\\\\\\\
+
+
+            Human human = new Human("Murat", "Arslan");
+
+            
+
+            Console.Read();
+
+        }
+    }
+}
